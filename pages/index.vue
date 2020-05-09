d5ccda9414cad15166fc1c3a6f6432cf1b1c7fe7
Rafi Halilintar <masitingss@gmail.com>
2020-05-10 00:39:53 +0700

    oke

--- pages/index.vue
+++ pages/index.vue
@@ -32,7 +32,7 @@ export default {
           }
       }
       try {
-          const res = await axios.get('http://power.vpnlabs.xyz/api/v1/campaign/'+ process.env.campaignid +'/wallpaper/keyword/100/done', config);
+          const res = await axios.get('https://power.vpnlabs.xyz/api/v1/campaign/'+ process.env.campaignid +'/wallpaper/keyword/100/done', config);
           this.contents = res.data.keyword;
           this.title = res.data.meta.dot;
           this.keyword = res.data.meta.comma;
